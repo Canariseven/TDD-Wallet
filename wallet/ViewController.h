e0494570b2324bfc6da3ce09605666336847cc43
Carmelo Ruymán Quintana Santana <canariseven@gmail.com>
2015-06-01 21:10:27 +0100

    Initial Commit

---
+++ wallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wallet
+//
+//  Created by Carmelo Ruymán Quintana Santana on 1/6/15.
+//  Copyright (c) 2015 Carmelo Ruymán Quintana Santana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wallet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  wallet
+//
+//  Created by Carmelo Ruymán Quintana Santana on 1/6/15.
+//  Copyright (c) 2015 Carmelo Ruymán Quintana Santana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
